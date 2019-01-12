b22a6124cc1ae1b96e97ae970f4180107e15605c
Danny Morgan <dan@dannydev.com>
2019-01-12 14:28:22 -0500

    Add dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `@xdmorgan/gatsby-starter`,
@@ -18,11 +22,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `@xdmorgan/gatsby-starter`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0068fa`,
+        theme_color: `#0068fa`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -31,6 +35,6 @@ module.exports = {
     `gatsby-plugin-typescript`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    // ...(process.env.NODE_ENV === 'production' ? ['gatsby-plugin-offline'] : []),
   ],
 }
