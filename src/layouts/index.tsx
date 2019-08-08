4089ed7ec9e86c8474f69615ca522fe410a54b2c
Dan Morgan <hello@danny.codes>
2019-08-07 22:14:24 -0400

    Add <SEO /> to global layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import { SEO } from '../components/seo'
 import Header from '../components/header'
 
 import '../styles/global.css'
 import '../styles/base-layout.css'
 
+const layoutQuery = graphql`
+  query SiteLayoutQuery {
+    site {
+      ...SiteMetaFields
+    }
+  }
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          ...SiteMetaFields
-        }
-      }
-    `}
+    query={layoutQuery}
     render={data => (
-      <div>
+      <>
+        <SEO title="Welcome" />
         <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
-      </div>
+      </>
     )}
   />
 )
